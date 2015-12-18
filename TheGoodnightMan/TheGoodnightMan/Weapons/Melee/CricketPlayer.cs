988dcd1393d01a6a308dab9be93261e78b3194b3
stetar <Steen_nisker@hotmail.com>
2015-12-18 13:40:34 +0100

    Revert "DO IT!"
    This reverts commit 8d7053aa2ba4f2ebfee7a9eeb2de344c577217d4.

---
+++ TheGoodnightMan/TheGoodnightMan/Weapons/Melee/CricketPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLoopOne.Weapons.Melee
+{
+    class CricketPlayer : Weapon
+    {
+        private static string imagePath = "";
+
+        public CricketPlayer(Vector2D startPos, float scaleFactor) : base(imagePath, startPos, scaleFactor)
+        {
+            damage = 3;
+            this.AttackSpeed = 1;
+            this.animationSpeed = 30;
+            meleeRange = 1.2f;
+        }
+    }
+}
---
+++ TheGoodnightMan/TheGoodnightMan/Weapons/Melee/Katana.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLoopOne.Weapons.Melee
+{
+    class Katana : Weapon
+    {
+        private static string imagePath = "";
+
+        public Katana(Vector2D startPos, float scaleFactor) : base(imagePath, startPos, scaleFactor)
+        {
+            damage = 5;
+            this.AttackSpeed = 0.5f;
+            this.animationSpeed = 30;
+            meleeRange = .8f;
+        }
+    }
+}
