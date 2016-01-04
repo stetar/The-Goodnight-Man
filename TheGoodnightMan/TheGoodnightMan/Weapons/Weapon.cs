3adfffe9cbd80ed4a388b0b7e02f163c7cbee300
stetar <Steen_nisker@hotmail.com>
2016-01-04 12:26:21 +0100

    .suo

--- TheGoodnightMan/TheGoodnightMan/Weapons/Weapon.cs
+++ TheGoodnightMan/TheGoodnightMan/Weapons/Weapon.cs
@@ -94,39 +94,27 @@ namespace GameLoopOne.Weapons
             switch (Player.weaponIndexNumber)
             {
                 case 11:
-
                     GameWorld.eng.Play2D("Machinegun.wav");
-
                     break;
 
                 case 12:
-
                     GameWorld.eng.Play2D("RPG.flac");
-
                     break;
 
                 case 13:
-
                     GameWorld.eng.Play2D("Submachinegun.wav");
-
                     break;
 
                 case 14:
-
                     GameWorld.eng.Play2D("LMG.wav");
-
                     break;
 
                 case 15:
-
                     GameWorld.eng.Play2D("Shotgun.wav");
-
                     break;
 
                 case 16:
-
                     GameWorld.eng.Play2D("GunshotPistol.wav");
-
                     break;
             }
             float x = position.X + 30;
