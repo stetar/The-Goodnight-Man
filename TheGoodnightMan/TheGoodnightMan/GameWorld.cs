d91f954f66ece8c35efc69f67f57dc4dd75fb04b
Maks Fredholm <maksprivat@hotmail.com>
2016-01-06 15:23:20 +0100

    commiting

--- TheGoodnightMan/TheGoodnightMan/GameWorld.cs
+++ TheGoodnightMan/TheGoodnightMan/GameWorld.cs
@@ -346,6 +346,7 @@ namespace GameLoopOne
                     //objects.Add(new Enemy("player/sprites/playersprite1.png", new Vector2D(770, 420), .75f));
                     //objects.Add(new Enemy("player/sprites/playersprite1.png", new Vector2D(300, 590), .75f));
                     objects.Add(new Bridge(new Vector2D(590, 435), .75f));
+                    objects.Add(new Enemy("player/sprites/playersprite1.png", new Vector2D(590, 518), .75f, (new AssaultRifle(new Vector2D(590, 518), .3f))));
                     break;
 
                 case 3:
@@ -361,6 +362,8 @@ namespace GameLoopOne
                     objects.Add(new Crate(new Vector2D(0, 464), .5f));
                     objects.Add(new Crate(new Vector2D(950, 590), .5f));
                     objects.Add(new Crate(new Vector2D(950, 508), .5f));
+                    objects.Add(new Enemy("player/sprites/playersprite1.png", new Vector2D(0, 217), .75f, (new AssaultRifle(new Vector2D(0, 217), .3f))));
+                    objects.Add(new Enemy("player/sprites/playersprite1.png", new Vector2D(950, 425), .75f, (new AssaultRifle(new Vector2D(950, 425), .3f))));
 
                     break;
 
@@ -380,6 +383,7 @@ namespace GameLoopOne
                     objects.Add(new Crate(new Vector2D(700, 150), .5f));
                     objects.Add(new Crate(new Vector2D(400, 200), .5f));
                     objects.Add(new Crate(new Vector2D(600, 450), .5f));
+                    objects.Add(new Enemy("player/sprites/playersprite1.png", new Vector2D(770, 417), .75f, (new AssaultRifle(new Vector2D(770, 417), .3f))));
 
                     break;
             }
