644a71efb567d44960ff1ab99271e223e407ae2a
stetar <Steen_nisker@hotmail.com>
2016-01-07 17:49:26 +0100

    Nye weapon klasser

---
+++ TheGoodnightMan/TheGoodnightMan/Weapons/EnemyOnly/EnemyPistol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLoopOne.Weapons.EnemyOnly
+{
+    class EnemyPistol : Weapon
+    {
+        private static string imagePath = "";
+        private static int weaponIndex;
+
+        EnemyPistol(Vector2D startPos, float scalefactor) : base(imagePath, startPos, scalefactor, weaponIndex)
+        {
+            damage = 3;
+            this.AttackSpeed = 1;
+        }
+    }
+}
---
+++ TheGoodnightMan/TheGoodnightMan/Weapons/EnemyOnly/EnemySMG.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLoopOne.Weapons.EnemyOnly
+{
+    class EnemySMG : Weapon
+    {
+        private static string imagePath = "";
+        private static int weaponIndex;
+
+        EnemySMG(Vector2D startPos, float scalefactor) : base(imagePath, startPos, scalefactor, weaponIndex)
+        {
+            damage = 3;
+            this.AttackSpeed = .4f;
+        }
+    }
+}
